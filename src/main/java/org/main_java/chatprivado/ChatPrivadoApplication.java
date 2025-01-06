bd722bdd9f537b8af81282c9d977f2365b03a9d9
Jaime López <jlopedia@myuax.com>
2025-01-06 16:30:55 +0100

    Initial commit

---
+++ src/main/java/org/main_java/chatprivado/ChatPrivadoApplication.java
@@ -0,0 +1,13 @@
+package org.main_java.chatprivado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPrivadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatPrivadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/main_java/chatprivado/ChatPrivadoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.main_java.chatprivado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPrivadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f8b6b69438bbc41c08e04b395ddbbbf01a580483
Jaime Lopez Diaz <jlopedia@myuax.com>
2025-01-11 17:33:18 +0100

    domain

--- src/main/java/org/main_java/chatprivado/domain/MensajePrivado.java
+++ src/main/java/org/main_java/chatprivado/domain/MensajePrivado.java
@@ -1,4 +1,41 @@
 package org.main_java.chatprivado.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "MensajePrivado")
+@Getter
+@Setter
 public class MensajePrivado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mensaje")
+    private String contendoCifrado;
+
+    @Column(name = "fecha")
+    private LocalDateTime fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_sala")
+    private SalaChatPrivado salaChatPrivado;
+
+    public MensajePrivado() {
+    }
+
+    public MensajePrivado(String contendoCifrado, LocalDateTime fecha, Usuario usuario, SalaChatPrivado salaChatPrivado) {
+        this.contendoCifrado = contendoCifrado;
+        this.fecha = fecha;
+        this.usuario = usuario;
+        this.salaChatPrivado = salaChatPrivado;
+    }
 }
--- src/main/java/org/main_java/chatprivado/domain/SalaChatPrivado.java
+++ src/main/java/org/main_java/chatprivado/domain/SalaChatPrivado.java
@@ -1,4 +1,33 @@
 package org.main_java.chatprivado.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "SalaChatPrivado")
+@Getter
+@Setter
 public class SalaChatPrivado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinColumn(name = "id_usuario")
+    private List<Usuario> usuarios;
+
+    @OneToMany(mappedBy = "salaChatPrivado")
+    private List<MensajePrivado> mensajesPrivados;
+
+    public SalaChatPrivado() {
+    }
+
+    public SalaChatPrivado(List<Usuario> usuarios, List<MensajePrivado> mensajesPrivados) {
+        this.usuarios = usuarios;
+        this.mensajesPrivados = mensajesPrivados;
+    }
 }
